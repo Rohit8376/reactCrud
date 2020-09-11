a192dc77cc536077c93986ff9d8dc8496fc71689
rohit pandey <1080pandey@gmail.com>
2020-09-11 17:30:32 +0530

    Updates

---
+++ precache-manifest.6463460ebc8a87a4aed946a90a2e9832.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "391608391232e3219e1bf9977a76a504",
+    "url": "/reactCrud/index.html"
+  },
+  {
+    "revision": "d07516dd11ced0b6e45e",
+    "url": "/reactCrud/static/css/main.46fe51b5.chunk.css"
+  },
+  {
+    "revision": "f8caddecb7b77a84184c",
+    "url": "/reactCrud/static/js/2.5d59d48c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/reactCrud/static/js/2.5d59d48c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d07516dd11ced0b6e45e",
+    "url": "/reactCrud/static/js/main.1f5434d0.chunk.js"
+  },
+  {
+    "revision": "5b3a79b8b557cb828d35",
+    "url": "/reactCrud/static/js/runtime-main.b8395bfc.js"
+  }
+]);
